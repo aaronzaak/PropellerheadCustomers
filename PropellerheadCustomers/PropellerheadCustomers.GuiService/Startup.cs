f4a2c840075e0fe378d8100ed813c5cd113e424a
Andrija Slezak <aslezak@gmail.com>
2018-04-05 02:42:54 +0200

    use of dev env var enabled

--- PropellerheadCustomers/PropellerheadCustomers.GuiService/Startup.cs
+++ PropellerheadCustomers/PropellerheadCustomers.GuiService/Startup.cs
@@ -28,7 +28,7 @@ namespace PropellerheadCustomers.GuiService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (true)//env.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
